5a5b5ac0b7034697b5bd914afcbbfa8724e58286
ajidp <126852346+ajidp@users.noreply.github.com>
2024-09-01 21:46:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -46,10 +46,10 @@ $(function () {
     albumArtworks = ["_1", "_2", "_3", "_4", "_5"],
     trackUrl = [
       "https://github.com/ajidp/ajid-notion-widget/raw/main/music/Old%20Letter.mp3",
-      "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/1.mp3",
-      "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/3.mp3",
-      "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/4.mp3",
-      "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/5.mp3"
+      "https://github.com/ajidp/ajid-notion-widget/raw/main/music/Old%20Letter.mp3",
+      "https://github.com/ajidp/ajid-notion-widget/raw/main/music/Old%20Letter.mp3",
+      "https://github.com/ajidp/ajid-notion-widget/raw/main/music/Old%20Letter.mp3",
+      "https://github.com/ajidp/ajid-notion-widget/raw/main/music/Old%20Letter.mp3"
     ],
     playPreviousTrackButton = $("#play-previous"),
     playNextTrackButton = $("#play-next"),
